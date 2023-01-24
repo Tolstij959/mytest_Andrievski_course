d01f1194fa964750e6106ecdf017510cb10711fb
John Doe <aleksandr-oskarev@yandex.ru>
2023-01-24 18:49:37 +0300

    Added comment in index.php

--- index.php
+++ index.php
@@ -2,7 +2,11 @@
 // Запросы к БД Mysql при помощи PHP(PDO)
 require_once 'config.php';
 //$connection = new PDO ("mysql:host=localhost;dbname=mytest;charset=utf8  ",username:"root", password:"mysql");
+//Сделал ниже так, потому что вариант через new PDO не работал(в сервере php 7.4 а в PHPStorm- 8.1, может поэтому)
 $dsn = 'mysql:host='.$host.';dbname='.$data;
 $pdo = new PDO($dsn, $user, $pass);
 $query = "INSERT INTO first ( `Фамилия`, `Имя`, `Отчество`, `Пол`, `Дата рождения`, `Статус контрагента`) VALUES ('Иванов','Сергей','Антонович','Мужской','2000-03-04','Покупатель')";
 $pdo->exec($query);
+
+
+
